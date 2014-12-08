246a4c011777e5d5c6429398227a49a2dab2795f
sunzhiwei <18610364019@163.com>
2014-12-08 09:37:33 +0800

    aaa :wq ccc#
    aqSSA:wq

---
+++ gmhx/src/main/java/com/gome/gmhx/entity/vo/HxRoleAuthorityVO.java
@@ -0,0 +1,44 @@
+package com.gome.gmhx.entity.vo;
+
+public class HxRoleAuthorityVO {
+
+	private String roleId;
+	
+	private String CategoryId;
+	
+	private String fittingAuthId;
+	
+	private String menuId;
+
+	public String getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(String roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getCategoryId() {
+		return CategoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		CategoryId = categoryId;
+	}
+
+	public String getFittingAuthId() {
+		return fittingAuthId;
+	}
+
+	public void setFittingAuthId(String fittingAuthId) {
+		this.fittingAuthId = fittingAuthId;
+	}
+
+	public String getMenuId() {
+		return menuId;
+	}
+
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+}
